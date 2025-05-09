344fd9645df05ddae0d98e621ecdae4ac4b1b8ce
DANILO DE SOUSA BRAZIL <danilobrazil0906@gmail.com>
2025-05-09 10:21:03 -0300

    Add files via upload

---
+++ Trabalho8.java
@@ -0,0 +1,30 @@
+/*Estrutura de repetição
+ *Danilo Brazil
+ * Anacleto tem 1,50m e cresce 2 centímetros por ano, enquanto Felisberto tem 1,10m e cresce 3 centímetros por ano. Construa um programa que calcula e apresenta quantos anos serão necessários para que Felisberto seja maior que Anacleto.
+ */
+package trabalho8;
+
+
+
+
+public class Trabalho8 {
+
+    public static void main(String[] args) {
+       
+        int soma = 0;
+        for (int i = 1; i <=100; i++){
+            soma += i;
+        }
+        System.out.println(soma);
+}
+}
+          
+           
+           
+           
+           
+           
+           
+           
+           
+
